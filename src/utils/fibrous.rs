36e2c54d3f504dbe40b29738aba27b67b91a1edc
jaykayudo <jaykayudo@gmail.com>
2025-02-04 08:08:07 +0100

    complete fibrous swap module

--- src/utils/fibrous.rs
+++ src/utils/fibrous.rs
@@ -9,22 +9,22 @@ use super::starknet::{contract_address_felt, signer_account};
 pub struct RouteParams {
     pub token_in: Felt,
     pub token_out: Felt,
-    pub amount_in: U256,
+    pub amount_in: u128,
     pub min_received: U256,
     pub destination: Felt,
 }
 
-#[derive(Debug, PartialEq, Eq, Clone, Encode, Decode)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct SwapParams {
     pub token_in: Felt,
     pub token_out: Felt,
     pub rate: u32,
     pub protocol_id: u32,
     pub pool_address: Felt,
-    pub extra_data: Felt,
+    pub extra_data: Vec<Felt>,
 }
 
-#[derive(Debug, PartialEq, Eq, Clone, Encode, Decode)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct CallData {
     route_params: RouteParams,
     swap_params: SwapParams,
@@ -38,29 +38,57 @@ type FibrousResponse = Result<
     >,
 >;
 
+fn encode_swap_params(swap_params: &Vec<SwapParams>) -> Vec<Felt> {
+    let swap_params_owned: Vec<SwapParams> = swap_params.to_owned();
+    swap_params_owned
+        .into_iter()
+        .flat_map(|params| {
+            let mut swap_data = vec![
+                params.token_in,
+                params.token_out,
+                Felt::from(params.rate),
+                Felt::from(params.protocol_id),
+                params.pool_address,
+                Felt::from(params.extra_data.len()),
+            ];
+            swap_data.extend(params.extra_data);
+            swap_data
+        })
+        .collect()
+}
+
 pub async fn fibrous_swap(
     route_params: RouteParams,
-    swap_params: SwapParams,
+    swap_params: Vec<SwapParams>,
     contract_address: Felt,
 ) -> FibrousResponse {
     let mut account = signer_account();
     let contract_address_main = contract_address_felt();
 
-    let call_data = CallData {
-        route_params,
-        swap_params,
-        contract_address,
-    };
     account.set_block_id(BlockId::Tag(BlockTag::Pending));
 
-    let mut serialized = vec![];
-    call_data.encode(&mut serialized).unwrap();
+    let encoded_swap_params = encode_swap_params(&swap_params);
+
+    let mut serialized: Vec<Felt> = vec![];
+    route_params.encode(&mut serialized).unwrap();
+    serialized.push(Felt::from(encoded_swap_params.len()));
+    serialized = serialized.into_iter().chain(encoded_swap_params).collect();
+    serialized.push(contract_address);
+
+    let approve_call = Call {
+        to: route_params.token_in,
+        selector: selector!("approve"),
+        calldata: vec![contract_address_main, route_params.amount_in.into()],
+    };
 
     let swap_call = Call {
         to: contract_address_main,
-        selector: selector!("swap"),
+        selector: selector!("fibrous_swap"),
         calldata: serialized,
     };
 
-    account.execute_v3(vec![swap_call]).send().await
+    account
+        .execute_v3(vec![approve_call, swap_call])
+        .send()
+        .await
 }
